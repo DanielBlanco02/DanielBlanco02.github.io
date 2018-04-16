e76a0a58c6af81a936a4d5e805ed69c03f8270c5
ContadeTeste021 <contadeteste021@gmail.com>
2018-04-15 21:21:50 -0300

    adicionada página de matrícula e suas regras em js, falta implementar css

---
+++ js/matricula.js
@@ -0,0 +1,21 @@
+function validar(){
+    
+            var inputs = document.getElementsByTagName("input")
+    
+            var i = 0, aprovados = 0, cancelados = 0;
+            for(i=0; i< inputs.length; i++){
+                if(inputs[i].id == "status"){
+                    if(inputs[i].value == "aprovado" && inputs[i].checked){
+                        aprovados++
+                    }
+                    if(inputs[i].value == "cancelado" && inputs[i].checked){
+                        cancelados++
+                    }
+                }
+            }
+    
+            
+    
+        confirm("Alunos aprovados: "+ aprovados + ", Alunos cancelados: " + cancelados)
+        }
+        
